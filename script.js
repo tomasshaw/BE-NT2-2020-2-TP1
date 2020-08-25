68d697f1085e11b626afca99944c090ad2b36f72
Thomas Shaw <tomasshaw@outlook.com>
2020-08-24 22:01:49 -0300

    feat: add task input text. add styles

--- script.js
+++ script.js
@@ -5,37 +5,60 @@ const classNames = {
 	TODO_DELETE: 'todo-delete',
 }
 
-
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+const taskInput = document.getElementById('task-input')
+
 let count = 0
+let checkedCount = 0
 
 const addCounter = () => {
 	++count
+	updateHTMLValues()
+}
+
+const updateHTMLValues = () => {
+	uncheckedCountSpan.innerHTML = `${count-checkedCount}`
 	itemCountSpan.innerHTML = `${count}`
 }
 
 function addTodo() {
-	const li = createListItem()
+	const li = createListItem(taskInput.value)
 	list.appendChild(li)
 	addCounter()
 }
 
-const createListItem = () => {
+const createListItem = (textValue) => {
 	const newLi = document.createElement('li')
 	newLi.className = classNames.TODO_TEXT
 	newLi.appendChild(createListItemCheckbox())
-	newLi.appendChild(createListItemText())
+	newLi.appendChild(createListItemText(textValue))
 	return newLi
+}
+
+const createListItemText = (textValue) => {
+	const tag = document.createElement('label')
+	tag.setAttribute('for', `${'item'+count}`)
+	tag.appendChild(document.createTextNode(textValue))
+	return tag
 }
 
 const createListItemCheckbox = () => {
 	const newCheckbox = document.createElement('input')
 	newCheckbox.setAttribute('type', 'checkbox')
+	newCheckbox.setAttribute('id', `${'item'+count}`)
+	newCheckbox.addEventListener('click', handleCheckboxToggle)
 	return newCheckbox
 }
 
-const createListItemText = () => {
-	return document.createTextNode('Nuevo item en la lista')
+const handleCheckboxToggle = ({target}) => {
+	if(target.checked){
+		++checkedCount
+		target.parentNode.classList.add('checked-text')
+	} else {
+		--checkedCount
+		target.parentNode.classList.remove('checked-text')
+	}
+	updateHTMLValues()
 }
